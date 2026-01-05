a1bae2f5981e6dad50a1e6a66bbace6815f41d27
Arnav Das <arnav.das88@gmail.com>
2026-01-05 19:20:06 +0000

    Setup updated

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ PYTHON_ENV="/usr/bin/env /home/codespace/.python/current/bin/python"
 $PYTHON_ENV -m pip install Cython --break-system-packages
 
 # Downloads WireguardPy
-cd /opt/ && git clone https://github.com/arnavdas88/wireguard_py
+cd /tmp/ && git clone https://github.com/arnavdas88/wireguard_py
 
 # Installs WireguardPy
-cd /opt/wireguard_py && \
+cd /tmp/wireguard_py && \
 sed -i "s/trunk/v0.0.1-dev0/g" wireguard_py/__init__.py && \
 $PYTHON_ENV setup.py sdist && \
 $PYTHON_ENV setup.py build_ext --inplace && \
